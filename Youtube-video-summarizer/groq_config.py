92bae22b7dfa0655b57221cdf18625365c9acb4d
Thirumurugan <112699713+Thirumurugan240@users.noreply.github.com>
2025-05-08 15:27:05 +0530

    Update groq_config.py

--- Youtube-video-summarizer/groq_config.py
+++ Youtube-video-summarizer/groq_config.py
@@ -3,6 +3,6 @@ from langchain_groq import ChatGroq
 def get_groq_llm():
     return ChatGroq(
         model_name = "llama3-8b-8192",
-        groq_api_key="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",
+        groq_api_key="your_api_key",
         temperature=0.5
     )
